f3b1610f1fbf8c0e3806821562f1bc605023f018
Gabriel <gabrielcn6@gmail.com>
2020-11-28 15:38:00 -0300

    Se agregaron: Fuentes Assets svgs propios Variables de fuentes y colores Se maqueto primera y segunda seccion de landing page Se hizo la primera parte del responsive de landing page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [CoreComponent],
+  declarations: [CoreComponent, HeaderComponent],
   imports: [
     CommonModule,
     BrowserModule,
